8e6426b947a95451add28742adaa693695af4d44
Jim Lester <jim@jimlester.net>
2014-04-17 22:56:36 -0400

    Game parsing and scores for schedule fill out

--- scoreboard.py
+++ scoreboard.py
@@ -33,20 +33,67 @@ def close_db(error):
 	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
 
-@app.template_filter('expand')
-def expand_game(g):
-	match = re.search( '^S(\d+)$' )
-	if match:
-		g = "Seed " . match.group(1)
+def get_team(team_id):
+	db = get_db()
+	cur = db.execute('SELECT name FROM teams WHERE team_id=? AND tid=?', (team_id, app.config['TID']))
+	team = cur.fetchone()
 
-	return g
+	return team['name']
+
+def parse_game(g):
+	game = g
+
+	match = re.search( '^T(\d+)$', g)
+	if match:
+		game = get_team(match.group(1))
+
+	match = re.search( '^S(\d+)$', g )
+	if match:
+		game = "Seed " + match.group(1)
+	
+	match = re.search( '^W(\d+)$', g)
+	if match:
+		game = "Winner of " + match.group(1)
+
+	match = re.search( '^L(\d+)$', g)
+	if match:
+		game = "Looser of " + match.group(1)
+
+	return game
+
+def expand_games(games):
+	expanded = []
+	db = get_db()
+	
+	for info in games:
+		game = {}
+		game["gid"] = info['gid']
+		game["day"] = info['day']
+		game["start_time"] = info['start_time']
+		game["black"] = parse_game(info['black'])
+		game["white"] = parse_game(info['white'])
+
+		cur = db.execute('SELECT score_b, score_w FROM scores WHERE gid=? AND tid=?', (game['gid'],app.config['TID']))
+		score = cur.fetchone()
+
+		if score:
+			game['score_b'] = score['score_b']		
+			game['score_w'] = score['score_w']
+		else:
+			game['score_b'] = "--"
+			game['score_w'] = "--"
+		
+		expanded.append(game)
+
+	return expanded 
+		
 
 
 @app.route('/')
 def render_main():
 	db = get_db()
 	cur = db.execute('SELECT gid, day, start_time, black, white FROM games WHERE tid=? ORDER BY gid',app.config['TID'])
-	games = cur.fetchall()
+	games = expand_games(cur.fetchall())
 
 	cur = db.execute('select name from teams WHERE tid=?', app.config['TID'])
 	teams = cur.fetchall()
