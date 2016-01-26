5319ee7d7e1619d8bd52b5ed2a4991b78fb37525
Jim Lester <jim@jimlester.net>
2016-01-26 11:48:54 -0600

    House cleaning

--- import.pl
+++ import.pl
@@ -3,31 +3,41 @@
 use DBI;
 #use strict;
 
-$tid=2;
+$tid=8;
 
-my $db = DBI->connect("dbi:SQLite:dbname=scores.db","","");
+my $db = DBI->connect("dbi:SQLite:dbname=../scores.db","","");
 
-open FILE, "schedule.csv" or die "cannot open file: $!";
+$db->do("DELETE FROM SCORES WHERE tid=$tid");
 
-$db->do("DELETE FROM games WHERE tid=$tid");
+if (-e "schedule.csv") {
+	open FILE, "schedule.csv" or die "cannot open file: $!";
 
-$header = <FILE>;
+	$db->do("DELETE FROM games WHERE tid=$tid");
 
-while (<FILE>) {
-	($gid,$day,$time,$pool,$black,$white,$div,$pod,$type)  = split(/,/, $_);
-	chomp $type;
-	$type =~ s/^\s+|\s+$//g;	
-	if ($type){
-		$add = $db->prepare("INSERT INTO games(tid, gid, day, start_time, pool, black, white, division, pod, type) VALUES(?,?,?,?,?,?,?,?,?,?)");
-		$add->execute($tid, $gid, $day, $time, $pool, $black, $white, $div, $pod, $type);		
-	} else{
-		$add = $db->prepare("INSERT INTO games(tid, gid, day, start_time, pool, black, white, division, pod) VALUES(?,?,?,?,?,?,?,?,?)");
-		$add->execute($tid, $gid, $day, $time, $pool, $black, $white, $division, $pod);		
+	$header = <FILE>;
+
+	while (<FILE>) {
+		($gid,$day,$time,$pool,$black,$white,$div,$pod,$type,$desc)  = split(/,/, $_);
+		chomp $type;
+		$type =~ s/^\s+|\s+$//g;
+		chomp $desc;
+		$desc =~ s/^\s+|\s+$//g;
+		$desc =~ s/\r|\n//g;
+
+		if ($time =~ /^\d\:\d\d/) {
+			$time="0$time"
+		}
+		if ($type){
+			$add = $db->prepare("INSERT INTO games(tid, gid, day, start_time, pool, black, white, division, pod, type,description) VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+			$add->execute($tid, $gid, $day, $time, $pool, $black, $white, $div, $pod, $type,$desc);
+		} else{
+			$add = $db->prepare("INSERT INTO games(tid, gid, day, start_time, pool, black, white, division, pod) VALUES(?,?,?,?,?,?,?,?,?)");
+			$add->execute($tid, $gid, $day, $time, $pool, $black, $white, $division, $pod);
+		}
 	}
+
+	close($fh);
 }
-
-close($fh);
-
 
 if ( -e "teams.csv" ) {
 	$db->do("DELETE FROM teams WHERE tid=$tid");
@@ -36,23 +46,25 @@ if ( -e "teams.csv" ) {
 	while (<FILE>) {
 		($team_id,$name,$division) = split(/,/, $_);
 		chomp $division;
-		
+		$division =~ s/^\s+|\s+$//g;
 		$add = $db->prepare("INSERT INTO teams(tid, team_id, name, division) VALUES(?,?,?,?)");
 		$add->execute($tid, $team_id, $name, $division);
 	}
 	close FILE;
 }
 
-if ( -e "places.csv" ) {
-	$db->do("DELETE FROM places WHERE tid=$tid");
-	open FILE, "places.csv" or die "cannot open file: $!";
+if ( -e "rankings.csv" ) {
+	$db->do("DELETE FROM rankings WHERE tid=$tid");
+	open FILE, "rankings.csv" or die "cannot open file: $!";
 
 	while (<FILE>) {
-		($place,$game) = split(/,/,$_);
-		$add = $db->prepare("INSERT INTO places(tid, place, game) VALUES(?,?,?)");
-		$add->execute($tid, $place, $game);	
+		($place,$game,$division) = split(/,/,$_);
+		chomp $division;
+
+		$add = $db->prepare("INSERT INTO rankings(tid, place, game, division) VALUES(?,?,?,?)");
+		$add->execute($tid, $place, $game, $division);
 	}
-	
+
 	close FILE;
 }
 
@@ -64,11 +76,30 @@ if ( -e "pods.csv"){
 	while(<FILE>){
 		($team_id,$pod) = split(/,/,$_);
 		chomp $pod;
-		$pod =~ s/^\r+|\r+$//g;	
+		$pod =~ s/^\r+|\r+$//g;
 		$add = $db->prepare("INSERT INTO pods(tid, team_id, pod) VALUES(?,?,?)");
 		$add->execute($tid, $team_id, $pod);
 	}
 	close FILE;
 }
 
+if (-e "groups.csv"){
+	$db->do("DELETE FROM groups WHERE tid=$tid");
 
+	open FILE, "groups.csv" or die "cannot open file: $!";
+
+	while(<FILE>){
+		($group_id,$name) = split(/,/,$_);
+		chomp $name;
+		$name =~ s/^\r+|\r+$//g;
+		$add = $db->prepare("INSERT INTO groups(tid,group_id,name) VALUES(?,?,?)");
+		$add->execute($tid,$group_id,$name);
+	}
+	close FILE;
+}
+$db->do("DELETE FROM params WHERE tid=$tid");
+
+#$add = $db->prepare("INSERT INTO params(tid, field, val) VALUES(?,?,?)");
+#$add->execute($tid,"seeded_pods","0");
+
+#$db->do("update games set division='O' where tid=6;");
--- schema.sql
+++ schema.sql
@@ -1,40 +1,26 @@
-drop table if exists games;
-drop table if exists scores;
-drop table if exists teams;
-drop table if exists tournaments;
-
-
 CREATE TABLE games(
-	tid INTEGER NOT NULL, gid INTEGER NOT NULL, day text, start_time datetime, pool text, black text, white text, type text, division text, pod text, description text, PRIMARY KEY (tid, gid)
+	tid INTEGER NOT NULL, gid INTEGER NOT NULL, day text, start_time time, pool text, black text, white text, type text, division text, pod text, description text, PRIMARY KEY (tid, gid)
 );
-
 CREATE TABLE scores(
-	tid INTEGER NOT NULL, gid INTEGER NOT NULL, black_tid INTEGER, white_tid INTEGER, score_b INTEGER, score_w INTEGER,
-	pod text, forfeit TEXT, PRIMARY KEY (tid,gid)
+	tid INTEGER NOT NULL, gid INTEGER NOT NULL, black_tid INTEGER, white_tid INTEGER, score_b INTEGER, score_w INTEGER, pod text, forfeit TEXT, PRIMARY KEY (tid,gid)
 );
-
-CREATE TABLE teams(
-	tid INTEGER, team_id INTEGER, name Text, division text, PRIMARY KEY(tid, team_id)
-);
-
 CREATE TABLE tournaments(
-	tid INTEGER PRIMARY KEY, name TEXT, short_name TEXT, start_date datetime, end_date datetime, location TEXT
+	tid INTEGER PRIMARY KEY, name TEXT, short_name TEXT
+, start_date datetime, end_date datetime, location TEXT);
+CREATE TABLE teams(
+tid INTEGER, team_id INTEGER, name Text, division text, PRIMARY KEY(tid, team_id)
 );
-
 CREATE TABLE pods(
-	tid INTEGER NOT NULL, team_id INTEGER, pod Text, pod_id INTEGER
+tid INTEGER, team_id INTEGER, pod Text, pod_id INTEGER
 );
-
 CREATE TABLE rankings(
-	tid INTEGER, place text NOT NULL, game text, division text, PRIMARY KEY(tid, place)
+        tid INTEGER NOT NULL, place text NOT NULL, game text, division text, PRIMARY KEY(tid, place)
 );
-
+CREATE INDEX time_index ON games (start_time);
 CREATE TABLE params(
-	tid INTEGER NOT NULL, field text NOT NULL, val NOT NULL
+tid INTEGER NOT NULL, field text NOT NULL, val NOT NULL
 );
-
 CREATE TABLE groups(
-	tid INTEGER NOT NULL, group_id text NOT NULL, name text NOT NULL, PRIMARY KEY(tid, group_id)
+tid INTEGER NOT NULL, group_id text NOT NULL, name text NOT NULL, PRIMARY KEY(tid, group_id)
 );
-
 CREATE UNIQUE INDEX unique_name on tournaments(short_name);
