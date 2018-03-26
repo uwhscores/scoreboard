eb230b1030254bc0aa1133078893f607895e7701
Jim Lester <jim@jimlester.net>
2018-03-25 22:41:27 -0500

    Moving to pulling db path out of os.environment instead of app.config

--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,10 @@
+import os
 from app import app
 from werkzeug.contrib.fixers import ProxyFix
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
+os.environ["SCOREBOARD_DB"] = "app/scores.db"
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0')
